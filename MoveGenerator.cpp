7ce6fa5b4f55fa69eb5ad134ea3a008557893f99
PortatilPROG\pc <gonzadb7@gmail.com>
2025-01-15 11:06:09 +0100

    MoveGenerator - Cambios en el comportamiento de generatePawnMoves y pruebas

--- MoveGenerator.cpp
+++ MoveGenerator.cpp
@@ -2,7 +2,7 @@
 
 std::vector<Move> MoveGenerator::generateMoves(const Board& board, int color) {
     std::vector<Move> moves;
-    //generatePawnMoves(board, moves, color);
+    generatePawnMoves(board, moves, color);
     generateBishopMoves(board, moves, color);
     return moves;
 }
@@ -27,38 +27,27 @@ void MoveGenerator::generatePawnMoves(const Board& board, std::vector<Move>& mov
 
 void MoveGenerator::generateWhitePawnsMoves(const Board& board, std::vector<Move>& moves) {
     uint64_t w_pawns = board.getBitboardFromType(board.W_PAWN);
-    uint64_t occupied = board.getOccupiedBitBoard();
-    uint64_t white = board.getWhiteBitBoard();
 
     for (int from = 0; from < 64; from++) {
-        uint64_t from_mask = Board::getBit(occupied, from);
+        uint64_t from_mask = Board::getBit(w_pawns, from);
         if (from_mask & w_pawns) {
-            uint64_t to_forward = from_mask << 8;
-            uint64_t to_forward_2 = from_mask << 16;
-            if (!(to_forward & occupied)) { // 1 square forward
-                if (!(to_forward_2 & occupied) && (from_mask & MoveGenerator::w_pawns_starting_pos)) { // 2 squares forward
-                    Move m = { from , from + 16, -1 };
-                    moves.push_back(m);
-                }
-                Move m = {from, from+8, -1};
-                moves.push_back(m);
-                //Falta añadir las promociones de los peones
-            }
+            int from_rank = from / 8;
+            int from_col = from % 8;
+            
+            Move fwd_1{ from, from + 8, -1 };
+            moves.push_back(fwd_1);
 
-            uint64_t left_forward = from_mask << 7;
-            uint64_t right_forward = from_mask << 9;
-            //Antes de comprobar la captura en diagonal, comprueba que el peón no se encuentre pegando al borde del tablero 
-            if (!(from_mask & Board::left)) {
-                //Comprueba que la casilla a la que se va a mover no esté ocupada por una pieza del mismo color o vacía
-                if (!(left_forward & white) && (left_forward & occupied)) {
-                    Move m { from, from + 7, -1 };
-                    moves.push_back(m);
-                }
-            } else if (!(from_mask & Board::right)) {
-                if (!(right_forward & white) && (right_forward & occupied)) {
-                    Move m { from, from + 9, -1 };
-                    moves.push_back(m);
-                }
+            if (from_rank == 1) {
+                Move fwd_2{ from, from + 16, -1 };
+                moves.push_back(fwd_2);
+            }
+            if (from_col < 7) {
+                Move right{ from, from + 9, -1 };
+                moves.push_back(right);
+            }
+            if (from_col > 0) {
+                Move left{ from, from + 7, -1 };
+                moves.push_back(left);
             }
         }
     }
@@ -66,38 +55,29 @@ void MoveGenerator::generateWhitePawnsMoves(const Board& board, std::vector<Move
 
 void MoveGenerator::generateBlackPawnMoves(const Board& board, std::vector<Move>& moves){
     uint64_t b_pawns = board.getBitboardFromType(board.B_PAWN);
-    uint64_t occupied = board.getOccupiedBitBoard();
-    uint64_t black = board.getBlackBitBoard();
 
     for (int from = 0; from < 64; from++) {
-        uint64_t from_mask = Board::getBit(occupied, from);
+        uint64_t from_mask = Board::getBit(b_pawns, from);
         if (from_mask & b_pawns) {
-            uint64_t to_forward = from_mask >> 8;
-            uint64_t to_forward_2 = from_mask >> 16;
-            if (!(to_forward & occupied)) {
-                if (!(to_forward_2 & occupied) && (from_mask & MoveGenerator::b_pawns_starting_pos)) { //2 squares forward
-                    Move m = { from , from - 16, -1 };
-                    moves.push_back(m);
-                }
-                Move m = { from, from - 8, -1 };
-                moves.push_back(m);
-                //Falta añadir las promociones de los peones
+            int from_rank = from / 8;
+            int from_col = from % 8;
+
+            Move fwd_1{ from, from - 8, -1 };
+            moves.push_back(fwd_1);
+
+            if (from_rank == 6) {
+                Move fwd_2{ from, from - 16, -1 };
+                moves.push_back(fwd_2);
             }
 
-            uint64_t left_forward = from_mask >> 9;
-            uint64_t right_forward = from_mask >> 7;
-            //Antes de comprobar la captura en diagonal, comprueba que el peón no se encuentre pegando al borde del tablero 
-            if (!(from_mask & Board::left)) {
-                //Comprueba que la casilla a la que se va a mover no esté ocupada por una pieza del mismo color o vacía
-                if (!(left_forward & black) && (left_forward & occupied)) {
-                    Move m{from , from-9, -1};
-                    moves.push_back(m);
-                }
-            } else if (!(from_mask & Board::right)) {
-                if (!(right_forward & black) && (right_forward & occupied)) {
-                    Move m{ from, from - 7, -1 };
-                    moves.push_back(m);
-                }
+            if (from_col < 7) {
+                Move right{ from, from - 7, -1 };
+                moves.push_back(right);
+            }
+
+            if (from_col > 0) {
+                Move left{ from, from - 9, -1 };
+                moves.push_back(left);
             }
         }
     }
