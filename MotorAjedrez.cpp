0bfcdae63bf2360cf0f002e9bfb341e3e59a398b
PortatilPROG\pc <gonzadb7@gmail.com>
2025-04-16 12:55:35 +0200

    main del motor listo para probarse con la api

--- MotorAjedrez.cpp
+++ MotorAjedrez.cpp
@@ -5,30 +5,65 @@
 #include <string>
 #include "Board.h"
 #include "MoveGenerator.h"
+#include "Evaluator.h"
 
 int main()
 {
-    const int WHITE = 0;
-    const int BLACK = 1;
+    const int MAKE_MOVE = 0;
+    const int GET_BEST_MOVE = 1;
+    const int GET_POSITION = 2;
+    const int GET_EVALUATION = 3;
+    const int RESTART = 4;
+    const int EXIT = -1;
 
     Board b = Board(false);
     MoveGenerator moveGen;
 
-    auto moves = moveGen.generateMoves(b, WHITE);
+    bool exit = false;
+    int option = 1;
+    std::string input;
 
-    for (auto& move : moves) {
-        std::string from = "From: " + std::to_string(move.from);
-        std::string to = " - To: " + std::to_string(move.to);
-        std::cout << from+to << std::endl;
+    while (!exit){
+        if (!std::getline(std::cin, input)) {
+            break;  // Fin de entrada (EOF)
+        }
+
+        try {
+            option = std::stoi(input);
+        }
+        catch (const std::exception& e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+            break;
+        }
+
+        if (std::cin.fail()) {
+            std::cout << "OUTPUT:INPUT_ERROR" << std::endl;
+            break;
+        }
+
+        switch (option) {
+        case 0:
+            std::cout << "MAKE_MOVE" << std::endl;
+            break;
+        case 1:
+            std::cout << "GET_BEST_MOVE" << std::endl;
+            break;
+        case 2:
+            std::cout << "GET_POSITION" << std::endl;
+            break;
+        case 3:
+            std::cout << "GET_EVALUATION" << std::endl;
+            break;
+        case 4:
+            std::cout << "RESTART" << std::endl;
+            break;
+        case -1:
+            std::cout << "EXIT" << std::endl;
+            exit = true;
+            break;
+        default:
+            std::cout << "DEFAULT" << std::endl;
+            break;
+        }
     }
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
